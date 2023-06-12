acd18eb07d7c99552eb8f9d9df38f0f46ebe407a
Thespica <3521897773@qq.com>
2023-06-12 16:32:56 +0800

    change TestClz

--- src/test/java/com/alibaba/graphar/utils/InfoVersionTest.java
+++ src/test/java/com/alibaba/graphar/utils/InfoVersionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class InfoVersionTest {
     @Test
     public void testInfoVersion() {
-        System.out.println(com.alibaba.graphar.utils.InfoVersion.creat(1).version());
+        InfoVersion infoVersion = InfoVersion.creat(1);
+        System.out.println(infoVersion.version());
     }
 }
